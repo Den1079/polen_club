3670adc98749a855a69766f830a5d56425c3dd93
Denis <94396112+Den1079@users.noreply.github.com>
2022-10-02 16:42:11 +0300

    Update pollen_club_parser2.py

--- pollen_club_parser2.py
+++ pollen_club_parser2.py
@@ -3,13 +3,14 @@ import requests
 import time
 
 token = '7b15c74f7b15c74f7b15c74f717805135b77b157b15c74f1825b71429e94b9034cf43e3'
-version = 5.131
-domain = 'pollen.club'
-count = 50 # кол-во постов мак можно 100
-offset = 0  # смещение для увелечения постов
+version = 5.131 # версия API
+domain = 'pollen.club'  # id группы 
+count = 100 # кол-во постов max можно 100
+offset = 1000  # смещение для увелечения постов, если надо больше 100
+# сбор постов
 all_post = []
-while offset < 50:
-    response_well = requests.get('https://api.vk.com/method/wall.get', # сбор постов
+while offset < 1000:
+    response_well = requests.get('https://api.vk.com/method/wall.get',
                             params = {'access_token':token,
                                       'v':version,
                                       'domain':domain,
@@ -21,20 +22,20 @@ while offset < 50:
     offset += 100
     all_post.extend(data_wel)
     time.sleep(0.5)
-
+#  поиск комментариев в постах группы
 all_comments = []
 for i in range(len(all_post)):
-    if all_post[i]['from_id'] == -87598739: #  поиск постов группы
-        response_comments = requests.get('https://api.vk.com/method/wall.getComments', #  сбор коментариев постов группы
-                                         params= {'access_token': token,
+    if all_post[i]['from_id'] == -87598739:
+        response_comments = requests.get('https://api.vk.com/method/wall.getComments',                                          params= {'access_token': token,
                                                   'v': version,
                                                   'owner_id': -87598739, # id группы
                                                   'post_id': all_post[i]['id'], # id поста
-                                                  'count': 100 # кол-во постов мак можно 100
+                                                  'count': 100 # кол-во постов max можно 100
                                                   }
                                          )
-        data_comments = response_comments.json()
+        data_comments = response_comments.json()['response']['items']
         all_comments.extend(data_comments)
+        time.sleep(0.5)
 
 
 with open("polen_club.csv", "w", encoding='utf8') as file:
@@ -42,6 +43,5 @@ with open("polen_club.csv", "w", encoding='utf8') as file:
     csv_file.writerow(('id','data','post'))
     for item in all_comments:
         csv_file.writerow((item['id'],item['date'],item['text']))
-print()
 
 
